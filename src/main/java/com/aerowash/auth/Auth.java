179ec8fa9c5048ced155cec2b1039fa424c24a6d
joelmathewthomas <proxy0@tutamail.com>
2025-11-14 18:07:12 +0530

    implement shared pade access

--- src/main/java/com/aerowash/auth/Auth.java
+++ src/main/java/com/aerowash/auth/Auth.java
@@ -7,6 +7,32 @@ import javax.servlet.http.HttpSession;
 
 public class Auth {
 
+	public static boolean checkSession(HttpServletResponse response, HttpSession session, String role) {
+		// response -> HttpServletResponse object
+		// session -> HttpSession object
+		// role -> target role for check
+		try {
+
+			if (session == null) {
+				response.sendRedirect("status?c=2&r=1");
+				return false;
+			} else if (session.getAttribute("role").equals("inactive")) {
+				response.sendRedirect("status?c=5&r=1");
+				return false;
+			} else if (role.equals("all")) {
+				return true;
+			} else if (!role.equals(session.getAttribute("role"))) {
+				response.sendRedirect("status?c=3");
+				return false;
+			}
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return true;
+
+	}
+
 	public static boolean checkSession(HttpServletResponse response, HttpSession session, String role, int code,
 			int route) {
 		// response -> HttpServletResponse object
@@ -22,6 +48,8 @@ public class Auth {
 			} else if (session.getAttribute("role").equals("inactive")) {
 				response.sendRedirect("status?c=5&r=1");
 				return false;
+			} else if (role.equals("all")) {
+				return true;
 			} else if (!role.equals(session.getAttribute("role"))) {
 				response.sendRedirect("status?c=" + code + "&r=" + route);
 				return false;
