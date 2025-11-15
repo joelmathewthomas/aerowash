209f8ed8482357134e84f869b71e246848dc0017
joelmathewthomas <proxy0@tutamail.com>
2025-11-15 15:31:49 +0530

    UI level access management for new expense items

--- src/main/java/com/aerowash/wash/WashCrudServlet.java
+++ src/main/java/com/aerowash/wash/WashCrudServlet.java
@@ -104,6 +104,7 @@ public class WashCrudServlet extends HttpServlet {
 					+ "\n");
 					
 					while(rs.next()) {
+						
 						out.println(""
 								+ "        <tbody>\n"
 								+ "          <tr>\n"
@@ -123,7 +124,7 @@ public class WashCrudServlet extends HttpServlet {
 								+ "            <td>" + rs.getString(8) + "</td>\n"
 								+ "\n"
 								+ "            <td style=\"white-space: nowrap\">\n"
-								+ "[" + "              <a href=\"expense?wid=" + rs.getInt(1) + "&tid=" + rs.getInt(9) + "\">Expense</a> ] \n");
+								+ "[" + "              <a href=\"expense?wid=" + rs.getInt(1) + "&tid=" + rs.getInt(9) + "&s=" + (rs.getString(7).equals("COMPLETE") ? 1 : 0) + "\">Expense</a> ] \n");
 							
 								if (rs.getString(7).equals("INCOMPLETE")) {
 									out.println("[" + "              <a href=\"washview?wid=1\">Mark as Paid</a> ]\n");
--- src/main/java/com/aerowash/wash/expense/ExpenseCrudServlet.java
+++ src/main/java/com/aerowash/wash/expense/ExpenseCrudServlet.java
@@ -44,9 +44,11 @@ public class ExpenseCrudServlet extends HttpServlet {
 			
 			int wash_id = 0;
 			int transaction_id = 0;
+			int payment_status = 0;
 			try {
 				wash_id = Integer.parseInt(request.getParameter("wid"));
 				transaction_id = Integer.parseInt(request.getParameter("tid"));
+				payment_status = Integer.parseInt(request.getParameter("s"));
 			} catch (NumberFormatException ex) {
 				response.sendRedirect("status?c=4&r=6&e=invalid_wash_id_or_transaction_id");
 				ex.printStackTrace();
@@ -71,8 +73,13 @@ public class ExpenseCrudServlet extends HttpServlet {
 					+ "      <h3>Menu</h3>\n"
 					+ "\n"
 					+ "      <ul style=\"line-height: 1.8; margin-left: 0; padding-left: 15px\">"
-					+ "        <li><a href=\"wash\">Wash</a></li>\n"
-					+ "        <li><a href=\"eadd?wid=" + wash_id + "&tid=" + transaction_id + "\">New item</a></li>\n"
+					+ "        <li><a href=\"wash\">Wash</a></li>\n");
+					
+					if (payment_status == 0) {
+						out.println("        <li><a href=\"eadd?wid=" + wash_id + "&tid=" + transaction_id + "\">New item</a></li>\n");
+					}
+					
+					out.println(""
 					+ "      </ul>\n"
 					+ "    </div>\n"
 					+ "\n"
@@ -90,8 +97,13 @@ public class ExpenseCrudServlet extends HttpServlet {
 					+ "            <th>Expense ID</th>\n"
 					+ "            <th>Expense Item</th>\n"
 					+ "            <th>Amount</th>\n"
-					+ "            <th>Expense Date</th>\n"
-					+ "            <th>Action</th>\n"
+					+ "            <th>Expense Date</th>\n");
+					
+					if (role.equals("admin") && payment_status == 0) {
+						out.println("            <th>Action</th>\n");
+					}
+					
+					out.println(""
 					+ "          </tr>\n"
 					+ "        </thead>");
 			
@@ -101,16 +113,16 @@ public class ExpenseCrudServlet extends HttpServlet {
 						+ "            <td>" + rs.getInt(1) + "</td>\n"
 						+ "            <td>" + rs.getString(3) + "</td>\n"
 						+ "            <td>" + rs.getFloat(4) + "</td>\n"
-						+ "            <td>" + rs.getDate(5) + "</td>\n"
-						+ "            <td style=\"white-space: nowrap\">\n"
-						+ "              [ <a href=\"washview?wid=1\">Edit</a> ] \n");
-						
-						if (role.equals("admin")) {
-							out.println("             [ <a href=\"washview?wid=1\">Delete</a> ]\n");
+						+ "            <td>" + rs.getDate(5) + "</td>\n");
+
+						if (role.equals("admin") && payment_status == 0) {
+							out.println(""
+							+ "            <td style=\"white-space: nowrap\">\n"
+							+ "             <a href=\"washview?wid=1\">Delete</a>\n"
+							+ "            </td>\n");
 						}
-						
+
 						out.println(""
-						+ "            </td>\n"
 						+ "          </tr>\n"
 						+ "        </tbody>");
 			}
